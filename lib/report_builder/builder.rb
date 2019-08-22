0e7667d5d113c568c337c597458a8a4faec0a76b
Damien Storm <damien.r.storm@gmail.com>
2019-08-22 11:25:39 -0400

    more debugging

--- lib/report_builder/builder.rb
+++ lib/report_builder/builder.rb
@@ -43,7 +43,7 @@ module ReportBuilder
       if options[:report_types].include? 'HTML'
         File.open(html_report_path + '.html', 'w') do |file|
           begin
-            content_to_write = get(groups.size > 1 ? 'group_report' : 'report').result(binding).force_encoding(Encoding::UTF_8)
+            content_to_write = get(groups.size > 1 ? 'group_report' : 'report').force_encoding(Encoding::UTF_8).result(binding)
             content_to_write.gsub!('  ', '').gsub!("\n\n", '')
             puts "Content encoding: #{content_to_write.encoding}"
             file.write(content_to_write.encode('UTF-8'))
